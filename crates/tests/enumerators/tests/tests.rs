f9e8544a96ee0056a856bfe424700808278ba0cd
Rafael Rivera <rafael@withinwindows.com>
2025-01-09 14:52:57 -0800

    multiarch

--- crates/tests/enumerators/tests/tests.rs
+++ crates/tests/enumerators/tests/tests.rs
@@ -10,12 +10,20 @@ use windows::{
 fn get_test_session() -> Result<IDiaSession> {
     unsafe {
         CoInitializeEx(None, COINIT_MULTITHREADED).ok()?;
-        let source: IDiaDataSource = microsoft_dia::helpers::NoRegCoCreate(
+        let path = if cfg!(target_arch = "x86_64") {
             s!(
-                r#"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\DIA SDK\bin\amd64\msdia140.dll"#
-            ),
-            &DiaSource,
-        )?;
+                r"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\DIA SDK\bin\amd64\msdia140.dll"
+            )
+        } else if cfg!(target_arch = "aarch64") {
+            s!(
+                r"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\DIA SDK\bin\arm64\msdia140.dll"
+            )
+        } else if cfg!(target_arch = "x86") {
+            s!(r"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\DIA SDK\bin\msdia140.dll")
+        } else {
+            panic!("Unsupported target architecture");
+        };
+        let source: IDiaDataSource = microsoft_dia::helpers::NoRegCoCreate(path, &DiaSource)?;
         let executable = std::env::current_exe().unwrap();
         source.loadDataForExe(&HSTRING::from(executable.as_os_str()), None, None)?;
         source.openSession()
